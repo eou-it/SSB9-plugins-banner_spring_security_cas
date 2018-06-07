289fe570c1d904838a94ca488eb5ae2f3fe7e186
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-06-07 14:45:41 +0530

    Migrate the cas descriptor file to app

--- src/main/groovy/banner/spring/security/cas/BannerSpringSecurityCasGrailsPlugin.groovy
+++ src/main/groovy/banner/spring/security/cas/BannerSpringSecurityCasGrailsPlugin.groovy
@@ -1,6 +1,14 @@
 package banner.spring.security.cas
 
-import grails.plugins.*
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.plugins.Plugin
+import grails.util.Holders
+//TODO After adding Banner_Core Dependency uncomment below lines.
+//import net.hedtech.banner.controllers.ControllerUtils
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
+
+import javax.servlet.Filter
 
 class BannerSpringSecurityCasGrailsPlugin extends Plugin {
 
@@ -51,6 +59,47 @@ Brief summary/description of the plugin.
 
     void doWithApplicationContext() {
         // TODO Implement post initialization spring config (optional)
+        // build providers list here to give dependent plugins a chance to register some
+        def conf = SpringSecurityUtils.securityConfig
+        if (!conf || !conf.cas.active) {
+            return
+        }
+        def providerNames = []
+
+
+        if (conf.providerNames) {
+            providerNames.addAll conf.providerNames
+        } else {
+            //TODO After adding Banner_Core Dependency uncomment below lines.
+            /*if(ControllerUtils.isGuestAuthenticationEnabled()){
+                providerNames = ['casBannerAuthenticationProvider','selfServiceBannerAuthenticationProvider','bannerAuthenticationProvider']
+            } else{*/
+                providerNames = ['casBannerAuthenticationProvider']
+           /* }*/
+        }
+        applicationContext.authenticationManager.providers = createBeanList(providerNames, applicationContext)
+
+        // Define the spring security filters
+        def authenticationProvider = Holders?.config?.banner.sso.authenticationProvider
+        LinkedHashMap<String, String> filterChain = new LinkedHashMap();
+        switch (authenticationProvider) {
+            case 'cas':
+                filterChain['/**/api/**'] = 'statelessSecurityContextPersistenceFilter,bannerMepCodeFilter,authenticationProcessingFilter,basicAuthenticationFilter,securityContextHolderAwareRequestFilter,anonymousProcessingFilter,basicExceptionTranslationFilter,filterInvocationInterceptor'
+                filterChain['/**/qapi/**'] = 'statelessSecurityContextPersistenceFilter,bannerMepCodeFilter,authenticationProcessingFilter,basicAuthenticationFilter,securityContextHolderAwareRequestFilter,anonymousProcessingFilter,basicExceptionTranslationFilter,filterInvocationInterceptor'
+                filterChain['/**'] = 'securityContextPersistenceFilter,logoutFilter,bannerMepCodeFilter,casAuthenticationFilter,authenticationProcessingFilter,securityContextHolderAwareRequestFilter,anonymousProcessingFilter,exceptionTranslationFilter,filterInvocationInterceptor'
+                break
+            default:
+                break
+        }
+
+        LinkedHashMap<RequestMatcher, List<Filter>> filterChainMap = new LinkedHashMap()
+        filterChain.each { key, value ->
+            def filters = value.toString().split(',').collect {
+                name -> applicationContext.getBean(name)
+            }
+            filterChainMap[new AntPathRequestMatcher(key)] = filters
+        }
+        applicationContext.springSecurityFilterChain.filterChainMap = filterChainMap
     }
 
     void onChange(Map<String, Object> event) {
