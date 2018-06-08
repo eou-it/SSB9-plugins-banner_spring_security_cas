816560b3e8fec8776aa28ba42c52e135e5bd40ce
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-06-08 15:01:41 +0530

    Adding COre dependency to HRU-7824

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -178,3 +178,27 @@ String loggingFileDir =  "target/logs"
 String logAppName = "banner-third-party-authenticator"
 String loggingFileName = "${loggingFileDir}/${logAppName}.log".toString()
 def environment = grails.util.Environment.current
+
+cache.use_second_level_cache = true
+cache.use_query_cache = false
+cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+singleSession = true // configure OSIV singleSession mode
+flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+        }
+    }
+    test {
+        dataSource {
+        }
+    }
+    production {
+        dataSource {
+        }
+    }
+}
